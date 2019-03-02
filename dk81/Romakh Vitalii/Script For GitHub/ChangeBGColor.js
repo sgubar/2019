78467c8c1d3edad8b55a107f8048cc4c04b53bd9
ThisNicknameIsTaken <32493975+ThisNicknameIsTaken@users.noreply.github.com>
2019-03-02 20:11:36 +0200

    Update ChangeBGColor.js
    Change background color of every element on GitHub

--- dk81/Romakh Vitalii/Script For GitHub/ChangeBGColor.js
+++ dk81/Romakh Vitalii/Script For GitHub/ChangeBGColor.js
@@ -1,1 +1,32 @@
+// ==UserScript==
+// @name         GitHub Background color changer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make GitHub colorful!
+// @author       BeHappy
+// @match        https://github.com/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+    ChangeColor("*");
+
+
+    function ChangeColor(elements){
+     var nodelist = document.querySelectorAll(elements);
+     nodelist.forEach(function(item, i, nodelist) {
+      item.style.backgroundColor = RandomColor();
+     });
+    }
+
+    function RandomColor(){
+    var red = Math.random() * (256 - 0) + 0;
+      var green = Math.random() * (256 - 0) + 0;
+      var blue = Math.random() * (256 - 0) + 0;
+      var color = "rgb("+ red + "," + green + "," + blue + ")";
+      return color;
+    }
+
+
+})();
