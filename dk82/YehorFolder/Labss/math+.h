5b9c497d9144282e0b3ce769f8200689e8efef68
MrGoshak12 <48066034+MrGoshak12@users.noreply.github.com>
2019-03-11 09:06:19 +0200

    Create math+.h

---
+++ dk82/YehorFolder/Labss/math+.h
@@ -0,0 +1,13 @@
+#ifndef MATH+_H_INCLUDED
+#define MATH+_H_INCLUDED
+#define ERROR (-1)
+
+#include <stdio.h>
+
+int Fact(int number);
+
+float Pow(float number, int power);
+
+int Sum(int limit);
+
+#endif // MATH+_H_INCLUDED
