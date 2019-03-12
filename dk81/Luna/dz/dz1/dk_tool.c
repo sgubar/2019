9b58a91ea12e218ab10c94dbfcaadbc80ad49dfe
Luna1XX <48049339+Luna1XX@users.noreply.github.com>
2019-03-12 10:56:43 +0300

    Update dk_tool.c

--- dk81/Luna/dz/dz1/dk_tool.c
+++ dk81/Luna/dz/dz1/dk_tool.c
@@ -1,15 +1,50 @@
-    int words(char myString[100])
+#include "dk_tool.h"
+	
+int words(char myString[100])
 {
-    int nospace = 0;
-    int space = 1;
-    int length = strlen(myString);
-	for (int i = 0; i<=length; i++)
-    {
-
+	int i;
+    int no = 0;
+    int space = 0;
+    int len = dlina(myString);
+    for (i = 0; i<=len; i++) 
+	{
         if (myString[i] == ' ' && myString[i+1] != ' ' )
         {
             space++;
         }
+        if ( space == 1 && i != 0 && no == 0)
+        { 
+	      	no = 1; 
+		    space = 0;
+		}
+        if ( i == 0 && myString[i] != ' ') 
+        {
+            no = 1;
+        }
+    if(len < 1 ) 
+    {
+     space = 0;
     }
-     return space;
+    else if( len != 0 && space == 0 && myString[len-1] != ' ') 
+    {
+      space = 1;
+    }
+    else if( myString[len-1] == ' ' || space == 0) 
+    {
+
+    }
+
+    else
+    {
+       space++;
+    }}
+    return space;
+}
+int dlina(char str[])
+{
+int i = 0;
+while (str[i]!= '\0')
+i++;
+ 
+ return i;  
 }
