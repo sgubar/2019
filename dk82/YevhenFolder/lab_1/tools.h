f9f9a7d4c1bb6ae2f7ed8e06c747932b153a6501
YevhenNysh <48062205+YevhenNysh@users.noreply.github.com>
2019-03-16 16:02:16 +0200

    Update tools.h

--- dk82/YevhenFolder/lab_1/tools.h
+++ dk82/YevhenFolder/lab_1/tools.h
@@ -1,1 +1,12 @@
+#ifndef tools_h
+#define tools_h
+#include <stdio.h>
 
+float variableA();
+float variableB();
+float variableC();
+float division(int a, int b, int c);
+float suma(int a);
+float stepin(int d, int a);
+
+#endif
