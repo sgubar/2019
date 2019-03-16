efcd8b55cbfe809ad7b7cd5a016b395c4b8266c9
YevhenNysh <48062205+YevhenNysh@users.noreply.github.com>
2019-03-16 16:00:59 +0200

    Update Sourse.c

--- dk82/YevhenFolder/lab_1/Sourse.c
+++ dk82/YevhenFolder/lab_1/Sourse.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "tools.h"
 
+int main(int argc, char *argv[])
+{
+	float a;
+	float b;
+	float c;
+	float x;
+	
+	a = variableA();
+	b = variableB();
+	c = variableC();
+		
+    x = division((int) a, (int) b, (int) c) + suma((int) a);
+
+    printf_s("Q =  %f\n", x);
+
+	system("PAUSE");
+
+	return 0;
+}
