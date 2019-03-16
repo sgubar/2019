9249e4505d7754a06289294b2df941893f2d30fc
YevhenNysh <48062205+YevhenNysh@users.noreply.github.com>
2019-03-16 16:01:49 +0200

    Update tools.c

--- dk82/YevhenFolder/lab_1/tools.c
+++ dk82/YevhenFolder/lab_1/tools.c
@@ -1,1 +1,71 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include "tools.h"
+	
+float variableA()
+{
+	float a;
+	
+		printf_s("Enter variable A\n");
+		scanf_s("%f", &a);
+	
+	return a;
+}
+float variableB()
+{
+	
+	float b;
+	
+		printf_s("Enter variable B\n");
+		scanf_s("%f", &b);
+	
+	return b;
+}
+float variableC()
+{
+	float c;
+	
+		printf_s("Enter variable C\n");
+		scanf_s("%f", &c);
+	
+	return c;
+}
+float division(int a, int b, int c)
+{
+	float m ;
+	float q = 100 - b - c;
 
+	if (q != 0)
+	{
+		q = q;
+	}
+	else
+	{
+		printf_s("Error\n");
+	}
+
+	m =   (a + b + c) / q;
+
+	return m;
+}
+
+float suma(int a)
+{
+	int lowerbound = 0;
+	int res = 0;
+
+	for (lowerbound = 0; lowerbound <= a; lowerbound++)
+	{
+		res = res + stepin(a, lowerbound);
+	}
+	return res;
+}
+float stepin(int a, int lowerbound)
+{
+	int t;
+	int y = a + lowerbound;
+	t = pow(2, y);
+	return t;
+}
