78d7695f69dec8503407398644fc7451dde1678a
trueDKstudent <48096277+trueDKstudent@users.noreply.github.com>
2019-03-18 21:45:43 +0200

    Update main_lab.c

--- dk81/Mjanovsky Vitaly/Lab_1/main_lab.c
+++ dk81/Mjanovsky Vitaly/Lab_1/main_lab.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include "functions.h"
 
+int main() 
+{
+	int A, B, C;
+	
+	printf("Enter number A: ");	
+	scanf("%i ", &A);
+	printf("Enter number B: ");
+	scanf("%i ", &B);
+	printf("Enter number C: ");
+	scanf("%i ", &C);
+	
+	if(B == 0)
+	{
+		printf("ERROR: division by zero!");
+	}else{
+		printf("%.3f ", A * sqrt(B+C) / B + sum_fact(C, A) );
+	}
+	return 0;
+}
