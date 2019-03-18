68be59748d083523ad7ad449c572b5fb894a23af
trueDKstudent <48096277+trueDKstudent@users.noreply.github.com>
2019-03-18 21:46:39 +0200

    Update functions.c

--- dk81/Mjanovsky Vitaly/Lab_1/functions.c
+++ dk81/Mjanovsky Vitaly/Lab_1/functions.c
@@ -1,1 +1,35 @@
+#include "functions.h"
 
+float sqrt(int n)
+{
+	float a=0.0;
+	while(a*a < n)
+	a += 1;
+		
+	while(a*a > n)
+	a -= 0.001;
+	
+	return a;
+}
+
+int factorial(int n)
+{
+	int a=1;
+	while(n != 0)
+	{
+		a *= n;
+		n--;
+	}
+	return a;
+}
+
+int sum_fact(int n, int f)
+{
+	int i=0, sum=0;
+	while(i<=n)
+	{
+		sum += factorial(f+i);
+		i++;
+	}
+	return sum;
+}
