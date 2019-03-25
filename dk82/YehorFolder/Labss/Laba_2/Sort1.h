a02cfa40dede0d21da09ba3be3f1515c4c5107ee
MrGoshak12 <48066034+MrGoshak12@users.noreply.github.com>
2019-03-25 08:51:34 +0200

    Rename Sort.h to Sort1.h

---
+++ dk82/YehorFolder/Labss/Laba_2/Sort1.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H_INCLUDED
+#define SORT_H_INCLUDED
+
+void fillArray(int arr[],int length);
+void printArray(int arr[],int length);
+void BubbleSort(int arr[],int length);
+void ChoiceSort(int arr[],int length);
+void InsertSort(int arr[],int length);
+
+
+#endif // SORT_H_INCLUDED
