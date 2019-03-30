aff0acb2c9b8c12626cad26a9ed28e9778acab43
Yura2486 <48062596+Yura2486@users.noreply.github.com>
2019-03-30 23:27:18 +0200

    Add files via upload

--- dk82/Yura2486Folder/dz2/main.c
+++ dk82/Yura2486Folder/dz2/main.c
@@ -3,13 +3,30 @@
 
 int main(int argc, const char * argv[])
 {
+	float area = 0;
 	Point A = {0,0};
 	Point B = {2,0};
-	Point C = {0,2};
-	Point D = {2,100};
-
-	Square *ABCD = createSquare(&A, &B, &C, &D);
-	printSquare(ABCD);
-
+	Point C = {2,1};
+	Point D = {1,1};
+	Point arrayOfPoint[2] = {C, D};
+	
+	Square *a = createSquare(&A, &B);
+	printSquare(a);
+	area = areaOfsquare(a);
+	printf("area = %.2f\n", area);
+	
+	Square *b = createSquareWithCoordinate(3, 7, 0, 5);
+	printSquare(b);
+	area = areaOfsquare(b);
+	printf("area = %.2f\n", area);
+	
+	Square* c = createSquareWithArrayOfPoint(arrayOfPoint);
+	printSquare(c);
+	area = areaOfsquare(c);
+	printf("area = %.2f\n", area);
+	
+	destroySquare(a);
+	destroySquare(b);
+	destroySquare(c);
 	return 0;
 }
---
+++ dk82/Yura2486Folder/dz2/square.h
@@ -0,0 +1,30 @@
+#ifndef square_h
+#define square_h
+#include <stdio.h>
+
+typedef struct tagPoint
+	{
+	int x;
+	int y;
+	}Point;
+
+typedef struct tagSquare
+	{
+	Point* A;
+	Point* B;
+	}Square;
+
+
+Square *createSquare(Point *A, Point *B);
+Square *createSquareWithCoordinate(int xA, int yA, int xB, int yB);
+Point *createPointWithCoordinate(int x, int y);
+Point *copyPointWithPoint(Point *aPoint);
+Square *createSquareWithArrayOfPoint(Point *arrayOfPoint);
+
+void destroySquare(Square *aSquare);
+void printSquare(Square *aSquare);
+float areaOfsquare(Square *aSquare);
+
+
+#endif
+
