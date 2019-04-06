708c6bae030473870a2bb2e67146d6ab9bf7207f
MrGoshak12 <48066034+MrGoshak12@users.noreply.github.com>
2019-04-07 00:30:27 +0300

    Update main.c

--- dk82/YehorFolder/MyDZ/DZ_2/main.c
+++ dk82/YehorFolder/MyDZ/DZ_2/main.c
@@ -7,25 +7,30 @@ int main()
 {
 
 
-Point array[3]={1,1,3,7,0,-1};
 Point A={1,1};
-Point B={3,7};
-Point C={0,-1};
+Point B={2,2};
+Point C={0,0};
+Point array[3]={6,-2,3,7,0,5};
 Triangle *MNP=CreateTriangleByArray(array);
 Triangle *ABC=CreateTriangleByPoints(&A,&B,&C);
-Triangle *XYZ=CreateTriangleByCoordinates(1,1,3,7,0,-1);
+Triangle *XYZ=CreateTriangleByCoordinates(6,7,-1,2,-1,9);
 
 printTriangle(MNP);
-printTriangle(ABC);
+if (NULL != ABC)
+    printTriangle(ABC);
 printTriangle(XYZ);
 double square1=TriangleSquare(MNP);
 printf("\n\nSquare of created Triangle1 is %.2f \n",square1);
-double square2=TriangleSquare(ABC);
-printf("\nSquare of created Triangle2 is %.2f \n",square2);
-double square3=TriangleSquare(MNP);
-printf("\nSquare of created Triangle1 is %.2f \n",square3);
+if (NULL != ABC)
+{
+    double square2=TriangleSquare(ABC);
+    printf("\nSquare of created Triangle2 is %.2f \n",square2);
+}
+double square3=TriangleSquare(XYZ);
+printf("\nSquare of created Triangle2 is %.2f \n",square3);
 destroyTriangle(MNP);
-destroyTriangle(ABC);
+if (NULL != ABC)
+    destroyTriangle(ABC);
 destroyTriangle(XYZ);
     return 0;
 }
