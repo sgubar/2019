97da5dbb6b7896b7f233888baf70f7d55dc3feb6
Mizraelly <48050224+Mizraelly@users.noreply.github.com>
2019-04-14 19:07:20 +0300

    Delete dk_tools.h

--- dk82/AleksD/labTwo/dk_tools.h
+++
@@ -1,8 +0,0 @@
-FILE* fo;
-#define dk_tools.h
-void bubbleSort(char* string1);
-void selectionSort(char* string2);
-void insertionSort(char* string3);
-void getMas(char* string);
-void writeInFileRand();
-void WriteEndInFile(char* string);
