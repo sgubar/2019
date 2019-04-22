8aaeb8c836f6dba6dcf634f0a82fa0df15b46114
Yura2486 <48062596+Yura2486@users.noreply.github.com>
2019-04-23 00:25:23 +0300

    Add files via upload

---
+++ dk82/Yura2486Folder/dz5/main.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include "tool.h"
+#include <string.h>
+
+int main(int argc, const char * argv[])
+{
+	FILE *fo = fopen("C:/Users/User/Documents/Dev-c++/dz4/test.json", "w+");
+	//float area = 0;
+
+	Array *theArray1 = createArray(3);
+	Array *theArray2 = createArray(3);
+	Array *theArray3 = createArray(10);
+	
+	Point A = {0,0};
+	Point B = {0,5};
+
+	Point C = {0,0};
+	Point D = {0,6};
+	
+	Point E = {0,0};
+	Point F = {0,3};
+	
+	Square AB = {&A, &B};
+	Square CD = {&C, &D};
+	Square EF = {&E, &F};
+	
+	addElement(theArray1, &AB);
+	addElement(theArray1, &CD);
+	addElement(theArray1, &EF);
+	addElement(theArray2, &AB);
+	addElement(theArray2, &CD);
+	addElement(theArray2, &EF);
+	
+	addElement(theArray3, &AB);
+	addElement(theArray3, &CD);
+	addElement(theArray3, &EF);
+	B.y = 6;
+	addElement(theArray3, &AB);
+	B.y = 8;
+	addElement(theArray3, &AB);
+	B.y = 8;
+	addElement(theArray3, &AB);
+	B.y = 8;
+	addElement(theArray3, &AB);
+	B.y = 8;
+	addElement(theArray3, &AB);
+	B.y = 14;
+	addElement(theArray3, &AB);
+	B.y = 25;
+	addElement(theArray3, &AB);
+	
+	
+	printArray(theArray1);
+	printArray(theArray2);
+	printArray(theArray3);
+	writeArrayToJSON(fo, theArray1);
+	writeArrayToJSON(fo, theArray2);
+	writeArrayToJSON(fo, theArray3);
+
+	bubbleSort(theArray1);
+	selectionSort(theArray2);
+	shellSort(theArray3, theArray3->number);
+	
+	printf("\n\nSorted Arrays of Squares\n");
+		
+	printArray(theArray1);
+	printArray(theArray2);
+	printArray(theArray3);
+	
+	fprintf(fo, "\n\n\n");
+	
+	writeArrayToJSON(fo, theArray1);
+	writeArrayToJSON(fo, theArray2);
+	writeArrayToJSON(fo, theArray3);
+	
+	int key;
+	printf("Enter key to search\n");
+	scanf("%d", &key);
+	int* result = linearSearch(theArray3, key);
+	if (result[0] == -1)
+		{
+			printf("Square not found");
+		}
+	else
+		{
+			int i = 0;
+			printf("Square has been found\nIndex(indices) of square(s): ");
+			while (result[i] != -1)
+				{
+					if (result[i+1] == -1)
+						{
+							printf("%d. ", result[i]);
+							break;
+						}
+					printf("%d, ", result[i]);
+					i++;
+				}
+		}
+	
+	
+	freeArray(theArray1);
+	freeArray(theArray2);
+	freeArray(theArray3);
+
+	fflush(fo);
+	fclose(fo);
+	
+	return 0;
+}
+
---
+++ dk82/Yura2486Folder/dz5/sort.c
@@ -0,0 +1,98 @@
+#include "tool.h"
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void bubbleSort(Array *anArray)
+	{
+		int i, j;
+		Square* temp;
+		
+		for(i = anArray->number - 1; i>0; i--)
+			{
+				for(j = 0; j<i; j++)
+					{
+						if (areaOfsquare(anArray->squares[j]) > areaOfsquare(anArray->squares[j+1]))
+							{
+								temp = anArray->squares[j];
+								anArray->squares[j] = anArray->squares[j+1];
+								anArray->squares[j+1] = temp;
+							}
+					}
+			}
+	}
+	
+void selectionSort(Array *anArray)
+	{
+		int i, j;
+		Square* temp;
+		int minIndex;
+		
+		for (i = 0; i < anArray->number - 1; i++)
+			{
+				minIndex = i;
+				
+				for (j = i + 1; j < anArray->number; j++)
+					{
+						if (areaOfsquare(anArray->squares[j]) < areaOfsquare(anArray->squares[minIndex]))
+							{
+								minIndex = j;
+							}
+					}
+				temp = anArray->squares[i];
+				anArray->squares[i] = anArray->squares[minIndex];
+				anArray->squares[minIndex] = temp;
+			}
+	}
+
+void insertionSort(Array *anArray)
+	{
+			int i, j;
+		Square* temp;
+	
+		for (i = 1; i < anArray->number; i++)
+		{
+			j = i;
+			while (j > 0 && areaOfsquare(anArray->squares[j-1]) < areaOfsquare(anArray->squares[j]))
+			{
+				temp = anArray->squares[j-1];
+				anArray->squares[j-1] = anArray->squares[j];
+				anArray->squares[j] = temp;
+				j--;
+			}
+		}
+	}
+
+void shellSort(Array *anArray, int aCount)
+{
+	int theInner = 0;
+	int theOuter = 0;
+	int theH = 1; // start h = 1
+	
+	//1. Calculate start value of h
+	while (theH <= aCount/3)
+	{
+		theH = theH*3 + 1; // 1, 4, 13, 40, 121, ...
+	}
+
+	//2. Sequental decrease h to 1
+	while (theH > 0)
+	{
+		for (theOuter = theH; theOuter < aCount; theOuter ++)
+		{
+			Square* theTmp = anArray->squares[theOuter];
+			theInner = theOuter;
+		
+			// the first sub-array {0, 4, 8}
+			while (theInner > theH-1 && areaOfsquare(anArray->squares[theInner - theH]) <= areaOfsquare(theTmp))
+			{
+				anArray->squares[theInner] = anArray->squares[theInner - theH];
+				theInner -= theH;
+			}
+		
+			anArray->squares[theInner] = theTmp;
+		}
+	
+		theH = (theH - 1) / 3; //decrease h
+	}
+}
