7ab4aff8c638922c2f072e7b8429198a64a65451
TheHundredBucks <48151636+TheHundredBucks@users.noreply.github.com>
2019-05-06 09:03:25 +0300

    Create header.h

---
+++ dk82/BobronnikovA.S./lab2_var2_sort1/header.h
@@ -0,0 +1,7 @@
+#pragma once
+
+int enter_arr(int *charst, int size);
+int bubblesort(int *charres, int size);
+int selectionsort(int *charres, int size);
+int insertionsort(int *charres, int size);
+int savetofile(int *charres, int size);
