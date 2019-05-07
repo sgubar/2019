4cb31af73044e88c5e442c6d9705a25d0ebb4730
Luna1XX <48049339+Luna1XX@users.noreply.github.com>
2019-05-07 09:40:57 +0300

    Create main.c

---
+++ dk81/Luna/lab2/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "luna_tool.h"
+
+int main()
+{
+
+    int quantity = 0, choice = 0;
+
+    printf("Specify the quantity : ");
+    scanf("%d", &quantity);
+    while(quantity <= 0)
+    {
+        printf("Your quantity : ");
+        scanf("%d", &quantity);
+    }
+    int mass[quantity];
+
+    random(mass, quantity);
+
+    Filewriter(mass, quantity, "files.txt");
+
+    printf("Specify the type:\n 1 - Bubble type\n 2 - Select type\n 3 - Insert  type\nYour type: ");
+
+    Type_chooser(mass, quantity, choice);
+
+    return 0;
+}
