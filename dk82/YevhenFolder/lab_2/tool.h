197a82ac533e3d48f903c9ef820fa94cc6adbb6b
YevhenNysh <48062205+YevhenNysh@users.noreply.github.com>
2019-05-11 16:25:28 +0300

    Create tool.h

---
+++ dk82/YevhenFolder/lab_2/tool.h
@@ -0,0 +1,16 @@
+#ifndef tool_h
+#define tool_h
+#include <stdio.h>
+
+FILE* fo;
+
+void fillArray(int arr[], int length);
+void printArray(int arr[], int length);
+void printFail(int arr[], int length);
+void BubbleSort(int arr[], int length);
+
+//void ChoiceSort(int arr[], int length);
+//void InsertSort(int arr[], int length);
+
+
+#endif 
