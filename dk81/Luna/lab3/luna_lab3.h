da6e048cd7ce57554131819cf3c28309b4944748
Luna1XX <48049339+Luna1XX@users.noreply.github.com>
2019-05-14 11:01:14 +0300

    Create luna_lab3.h

---
+++ dk81/Luna/lab3/luna_lab3.h
@@ -0,0 +1,15 @@
+#ifndef LUNA_LAB3_H_INCLUDED
+#define LUNA_LAB3_H_INCLUDED
+#include <stdio.h>
+#include <time.h>
+
+void ShellSort(char *mass, int Count);
+
+void WritetoFile(char *mass, int asize, char *fname);
+
+int lineSearch(char Value, char *mass, int asize);
+
+void randomizer(char *mass, int asize);
+
+
+#endif // LUNA_LAB3_H_INCLUDED
