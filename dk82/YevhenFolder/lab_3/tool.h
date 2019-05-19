35acc9b7f3fffab2084018bed8477c232a5499e0
YevhenNysh <48062205+YevhenNysh@users.noreply.github.com>
2019-05-19 12:02:13 +0300

    Create tool.h

---
+++ dk82/YevhenFolder/lab_3/tool.h
@@ -0,0 +1,15 @@
+#ifndef tool_h
+#define tool_h
+#include <stdio.h>
+
+FILE* fo;
+
+void fillArray(int arr[], int length);
+void printArray(int arr[], int length);
+void printFail(int arr[], int length);
+void shellSort(int arr[], int length);
+int linSearch(int arr[], int length, int key);
+
+
+
+#endif
