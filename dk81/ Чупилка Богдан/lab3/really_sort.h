db92fb85bed7ca9a2b00956453ba6032149e3fda
Bohdan2019 <48049224+Bohdan2019@users.noreply.github.com>
2019-05-20 14:57:29 +0300

    Add files via upload

---
+++ dk81/ Чупилка Богдан/lab3/really_sort.h
@@ -0,0 +1,10 @@
+#ifndef REALLY_SORT_H_INCLUDED
+#define REALLY_SORT_H_INCLUDED
+
+#include <stdio.h>
+
+int read(char mainfile[], char str[] );
+void quickSort(char *items, int left, int right);
+int binarySearch(int a, int *str, int n);
+
+#endif
