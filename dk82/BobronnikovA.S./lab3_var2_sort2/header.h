b45b8fc15b1cdeb2bb2a0080ae1d04107f553331
TheHundredBucks <48151636+TheHundredBucks@users.noreply.github.com>
2019-05-20 09:46:07 +0300

    Create header.h

---
+++ dk82/BobronnikovA.S./lab3_var2_sort2/header.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int enter_arr(int *charres, int size);
+int quicksort(int *charres, int index1, int index2);
+int binarysearch(int tofind, int *charres, int size);
+int savetofile(int *charres, int size);
