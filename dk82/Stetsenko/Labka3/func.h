58c941bedeeb4640884fb3bef7fc569025b83aa1
Stdanya <48049328+Stdanya@users.noreply.github.com>
2019-05-27 10:58:55 +0300

    Create func.h

---
+++ dk82/Stetsenko/Labka3/func.h
@@ -0,0 +1,15 @@
+#ifndef LAB3_FUNC_H_INCLUDED
+#define LAB3_FUNC_H_INCLUDED
+#include <stdio.h>
+#include <time.h>
+
+
+
+void QuickSort( char *, int , int );
+void Record_File(char *, int , char *);
+int biSearch(char , char *, int );
+void random(char *, int );
+void swap(char *one, char *two);
+
+
+#endif // LAB3_FUNC_H_INCLUDED
