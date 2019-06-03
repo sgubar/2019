35d05630ba6a303ff606cc96e4c215fd8e97025c
SteelEyes <kolyf01@gmail.com>
2019-06-03 10:28:37 +0300

    Update main.c

--- dk82/M.Lutsky/Lab/lab5/main.c
+++ dk82/M.Lutsky/Lab/lab5/main.c
@@ -1,28 +1,12 @@
-#include "tool.h"
+#include "tree.h"
 
 void main() {
-
-	FILE *fp;
-	fopen_s(&fp, "text.txt", "a+");
-
-	char *mass = readFile(fp);
-	int size = fileSize(fp);
-	fclose(fp);
-
-	fopen_s(&fp, "text1.txt", "w+");
-	fprintf(fp, "Input: %s\n", mass);
-	clock_t inStart = clock();
-	shellSort(mass, size);
-	clock_t inEnd = clock();
-	printf("Time of Shell sorting: %f seconds, %d steps\n", (double)(inEnd - inStart) / CLOCKS_PER_SEC, inEnd - inStart);
-	printf("Result: %s\n", mass);
-	fprintf(fp, "Result: %s\n", mass);
-
-	char c = 'b';
-	int index = linearSearch(mass, c, size);
-	printf("Index for %c = %i\n", c, index);
-	fprintf(fp, "Index for %c = %i\n", c, index);
-	fclose(fp);
+	TreePtr theTree = createTree();
+	insertNode(theTree, "aab");
+	insertNode(theTree, "aaa");
+	insertNode(theTree, "acc");
+	insertNode(theTree, "baa");
+	displayTree(theTree->root);
 	system("pause");
 	return 0;
 }
