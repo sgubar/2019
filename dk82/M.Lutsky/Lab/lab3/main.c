0fb138d8697f95b72d5029e8ce9640c6af4459a6
SteelEyes <kolyf01@gmail.com>
2019-06-03 10:22:57 +0300

    Update main.c

--- dk82/M.Lutsky/Lab/lab3/main.c
+++ dk82/M.Lutsky/Lab/lab3/main.c
@@ -2,22 +2,27 @@
 
 void main() {
 
-
 	FILE *fp;
 	fopen_s(&fp, "text.txt", "a+");
 
 	char *mass = readFile(fp);
-	fseek(fp, 0l, SEEK_END);
-	int fileSize = ftell(fp);
+	int size = fileSize(fp);
 	fclose(fp);
 
-	shellSort(mass, fileSize);
+	fopen_s(&fp, "text1.txt", "w+");
+	fprintf(fp, "Input: %s\n", mass);
+	clock_t inStart = clock();
+	shellSort(mass, size);
+	clock_t inEnd = clock();
+	printf("Time of Shell sorting: %f seconds, %d steps\n", (double)(inEnd - inStart) / CLOCKS_PER_SEC, inEnd - inStart);
 	printf("Result: %s\n", mass);
+	fprintf(fp, "Result: %s\n", mass);
 
-	char c = '1';
-	int index = linearSearch(mass, c, fileSize);
+	char c = 'b';
+	int index = linearSearch(mass, c, size);
 	printf("Index for %c = %i\n", c, index);
-
+	fprintf(fp, "Index for %c = %i\n", c, index);
+	fclose(fp);
 	system("pause");
 	return 0;
 }
